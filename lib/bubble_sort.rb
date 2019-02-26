7b91df539b86821cf2459e23647f7b6dab163c73
kawanaka <kawanaka_s@tenso.com>
2019-02-26 15:37:56 +0900

    fixup! bubble_sortアルゴリズム作成

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,19 +1,19 @@
 class BubbleSort
   # sortメソッドここから
-  def sort(numeric_array)
+  def sort(numbers)
     # countは繰り返した回数
     # countに0を代入
     count = 0
     # countの数値が要素数よりも小さい間、処理を繰り返す
-    while count < numeric_array.size
+    while count < numbers.size
       # indexに0を代入
       index = 0
       # indexの数値が要素数-1より小さい間、処理を繰り返す
-      while index < numeric_array.size - 1
+      while index < numbers.size - 1
         # 隣り合った要素で大小を比較して、右の要素の方が小さい時
-        if numeric_array[index] > numeric_array[index + 1]
+        if numbers[index] > numbers[index + 1]
           # 要素を入れ替える
-          numeric_array[index], numeric_array[index + 1] = numeric_array[index + 1], numeric_array[index]
+          numbers[index], numbers[index + 1] = numbers[index + 1], numbers[index]
         # ifここまで
         end
         # indexを1増やす
@@ -25,7 +25,7 @@ class BubbleSort
     # whileここまで
     end
     # 要素を昇順に並べ替えた配列を返す
-    numeric_array
+    numbers
   # sortメソッドここまで
   end
 end
--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 require "bubble_sort"
 
-numeric_array= [5, 3, 1, 4, 2]
+numbers = [5, 3, 1, 4, 2]
 
 describe BubbleSort do
   describe "#sort" do
     context "隣り合った要素で大小を比較する" do
       it "要素を昇順に並べ替えた配列を返す" do
         bubble_sort = BubbleSort.new
-        expect(bubble_sort.sort(numeric_array)).to eq [1, 2, 3, 4, 5] 
+        expect(bubble_sort.sort(numbers)).to eq [1, 2, 3, 4, 5] 
       end
     end
   end
