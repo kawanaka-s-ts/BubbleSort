3c0d740a421a1e4a5916fe812d7b3c7bc55b6bd4
kawanaka <kawanaka_s@tenso.com>
2019-02-27 10:55:47 +0900

    fixup! fixup! bubble_sortアルゴリズム作成
    - コメント修正

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -4,7 +4,7 @@ require "bubble_sort"
 numbers = [*1..10].sort_by{rand}
 
 describe BubbleSort do
-  describe "#sort" do
+  describe "#ascending_order" do
     it "要素を昇順に並べ替えた配列を返す" do
       bubble_sort = BubbleSort.new
       expect(bubble_sort.ascending_order(numbers)).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
