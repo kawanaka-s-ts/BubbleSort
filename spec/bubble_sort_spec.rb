c4a249dff9881c1a3046ec82038aead4909a3b49
kawanaka <kawanaka_s@tenso.com>
2019-02-27 10:00:30 +0900

    fixup! bubble_sortアルゴリズム作成
    - 不要なcontext削除

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -5,11 +5,9 @@ numbers = [*1..10].sort_by{rand}
 
 describe BubbleSort do
   describe "#sort" do
-    context "隣り合った要素で大小を比較する" do
-      it "要素を昇順に並べ替えた配列を返す" do
-        bubble_sort = BubbleSort.new
-        expect(bubble_sort.sort(numbers)).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
-      end
+    it "要素を昇順に並べ替えた配列を返す" do
+      bubble_sort = BubbleSort.new
+      expect(bubble_sort.sort(numbers)).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
     end
   end
 end
