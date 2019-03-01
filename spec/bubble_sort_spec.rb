fc9d8938ecb57acd0c722f8146b750979ca6039f
kawanaka <kawanaka_s@tenso.com>
2019-03-01 15:40:41 +0900

    fixup! fixup! asc_orderメソッド、desc_orderメソッド追加
    - letを使ってテストコードの重複を修正

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 require "bubble_sort"
 
 describe BubbleSort do
-  describe "#change_order" do
+  let(:bubble_sort) {BubbleSort.new}
+  let(:numbers) {[*1..10].shuffle}
+  describe "#asc_order" do
     it "要素を昇順に並べ替えた配列を返す" do
-      bubble_sort = BubbleSort.new
-      numbers = [*1..10].shuffle
       expect(bubble_sort.asc_order(numbers)).to eq [*1..10]
     end
+  end
 
+  describe "#desc_order" do
     it "要素を降順に並べ替えた配列を返す" do
-      bubble_sort = BubbleSort.new
-      numbers = [*1..10].shuffle
       expect(bubble_sort.desc_order(numbers)).to eq [*1..10].reverse
     end
   end
